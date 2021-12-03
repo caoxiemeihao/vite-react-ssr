98e5fcda713fedbf9597f3ad941fd8aa8b245b9e
草鞋没号 <308487730@qq.com>
2021-12-03 09:43:10 +0800

    feat: integration antd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    styleImport({
+      libs: [{
+        libraryName: 'antd',
+        esModule: true,
+        resolveStyle: (name) => `antd/es/${name}/style/index`,
+      },],
+    }),
+  ],
   build: {
     minify: false,
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
   },
 })
